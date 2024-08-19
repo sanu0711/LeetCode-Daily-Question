79a167ab38788ed1b68209aa05db41287eff501c
ABHISHEK YADAV <sanuabhi.0711@gmail.com>
2024-08-19 14:36:37 +0530

    Create aug_19.py

---
+++ August/aug_19.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/2-keys-keyboard/description/
+class Solution:
+    def minSteps(self, n: int) -> int:
+        copy = 0
+        paste = 1
+        operation = 0
+
+        while paste != n:
+
+            if n % paste == 0:
+                copy = paste
+                operation += 1
+
+            paste += copy
+            operation += 1
+
+        return operation
